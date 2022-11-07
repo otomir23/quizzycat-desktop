34860859b22c3d7e3c45db1e580f72017b5dfcc9
Damir Modyarov <otomir@yandex.ru>
2022-11-07 21:47:22 +0300

    Add function to go to previous page

--- page.py
+++ page.py
@@ -4,10 +4,12 @@ from PyQt5 import uic
 
 class PageContainer(QWidget):
     _page = None
+    _previousPage = None
 
     def setPage(self, page):
         if self._page:
             self._page.setParent(None)
+        self._previousPage = self._page
         self._page = page
         self._page.setParent(self)
         self._page.setGeometry(0, 0, self.width(), self.height())
@@ -18,12 +20,16 @@ class PageContainer(QWidget):
             else 'quizzycat Desktop'
         )
 
+    def previousPage(self):
+        if self._previousPage:
+            self.setPage(self._previousPage)
+            self._previousPage = None
+
     def resizeEvent(self, event):
         if self._page:
             self._page.setGeometry(0, 0, self.width(), self.height())
 
         super().resizeEvent(event)
-
 
 class Page(QWidget):
     def __init__(self, parent: PageContainer):
